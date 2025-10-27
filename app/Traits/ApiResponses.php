05aedcf8fc359974ec81d4a2c1d289bb3250f000
Reiduarig <giraudier82@gmail.com>
2025-10-27 14:09:04 +0100

    agregar funcion error y modificar la success en el trait de respuestas de api

--- app/Traits/ApiResponses.php
+++ app/Traits/ApiResponses.php
@@ -1,12 +1,24 @@
 <?php  
 
 namespace App\Traits;
+use Illuminate\Http\JsonResponse;
 
 trait ApiResponses {
-    protected function success($message, $statusCode = 200) {
+
+    protected function success(string $message, $data = [], $statusCode = 200): JsonResponse
+    {
         return response()->json([
+            'status' => $statusCode,
             'message' => $message,
-            'status' => $statusCode
+            'data' => $data,
+        ], $statusCode);
+    }
+
+    protected function error($message, $statusCode): JsonResponse
+    {
+        return response()->json([
+            'status' => $statusCode,
+            'message' => $message,
         ], $statusCode);
     }
 }
