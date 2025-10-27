32688cd0433dd557c26c5889a48f21d183a0c162
Reiduarig <giraudier82@gmail.com>
2025-10-27 11:06:24 +0100

    trait para las respuestas de la api

---
+++ app/Traits/ApiResponses.php
@@ -0,0 +1,12 @@
+<?php  
+
+namespace App\Traits;
+
+trait ApiResponses {
+    protected function success($message, $statusCode = 200) {
+        return response()->json([
+            'message' => $message,
+            'status' => $statusCode
+        ], $statusCode);
+    }
+}
