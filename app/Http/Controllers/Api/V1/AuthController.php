efea4ab902f658c7b1312c29b6e8e8480841bbf1
Reiduarig <giraudier82@gmail.com>
2025-10-27 14:09:59 +0100

    implementacion funcion login y logout en authcontroller

--- app/Http/Controllers/Api/V1/AuthController.php
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -2,22 +2,65 @@
 
 namespace App\Http\Controllers\Api\V1;
 
+use App\Http\Controllers\Api\V1\Controller;
 use App\Traits\ApiResponses;
 use Illuminate\Http\Request;
-use App\Http\Requests\Api\V1\ApiLoginRequest;
+use App\Http\Requests\Api\V1\Auth\LoginRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 
 class AuthController extends Controller
 {
     use ApiResponses;
 
-    public function login(ApiLoginRequest $request)
+    public function login(LoginRequest $request)
     {
-        return $this->success($request->get('email'));
+        // Validar las credenciales del usuario
+        $credentials = $request->only('email', 'password');
+
+        if (! auth()->attempt([
+            'email' => data_get($credentials, 'email'),
+            'password' => data_get($credentials, 'password'),
+        ])) 
+        {
+            return $this->error('Credenciales incorrectas', 401);
+        }
+
+        return $this->success(
+            'Autenticado correctamente',
+            [
+                'token' => auth()->user()->createToken(
+                    'API Token for ' . auth()->user()->email,
+                    ['*'],
+                    now()->addMonth())->plainTextToken,
+            ]
+        );
     }
 
     public function register()
     {
         return $this->success('register');
     }
+
+    // Revocar el token de acceso actual
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return $this->success(
+            'Sesión cerrada correctamente',
+        );
+    }
+
+    // Revocar todos los tokens de acceso del usuario, esto cerrará sesión en todos los dispositivos
+    public function logoutAllDevices(Request $request)
+    {
+        $request->user()->tokens()->delete();
+
+        return $this->success(
+            'Sesión cerrada en todos los dispositivos correctamente',
+            []
+        );
+    } 
 }
