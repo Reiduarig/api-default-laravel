abd40bbc3aabd7fbd296abebbd44fb27f221b740
Reiduarig <giraudier82@gmail.com>
2025-10-27 17:13:21 +0100

    corregir los namespaces y corregir grupos de rutas

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -2,8 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\V1\AuthController;
-use App\Http\Controllers\Api\V1\TicketController;
+use App\Http\Controllers\API\V1\AuthController;
+use App\Http\Controllers\API\V1\UserController;
+use App\Http\Controllers\API\V1\TicketController;
 
 
 Route::prefix('auth')->group(function () {
@@ -15,9 +16,15 @@ Route::prefix('auth')->group(function () {
 
 Route::middleware('auth:sanctum')->group(function () {
 
-    Route::apiResource('tickets', TicketController::class);
+    Route::apiResource('users', UserController::class)
+        ->only(['index', 'show'])
+        ->name('*', 'users');
 
-    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::apiResource('tickets', TicketController::class)
+        ->only(['index', 'show'])
+        ->name('*', 'tickets');
+
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 });
 
