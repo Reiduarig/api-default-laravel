a8e51358cff8f433336ec6401bdffafc2e695d1e
Reiduarig <giraudier82@gmail.com>
2025-10-27 11:05:53 +0100

    rutas para login y register

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,16 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+
+Route::get('/', function () {
+    return response()->json(['message' => 'API is working'], 200);
+});
+
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/register', [AuthController::class, 'register'])->name('register');
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
