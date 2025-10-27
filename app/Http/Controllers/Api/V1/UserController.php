c27b5b21e79ee2b3d47286b6fcadcb77ea9b608f
Reiduarig <giraudier82@gmail.com>
2025-10-27 17:10:48 +0100

    crear user controller

---
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\API\V1;
+
+use App\Models\User;
+use App\Http\Resources\API\V1\UserResource;
+use App\Http\Requests\API\V1\StoreUserRequest;
+use App\Http\Requests\API\V1\UpdateUserRequest;
+
+class UserController
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return UserResource::collection(User::paginate());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreUserRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(User $user)
+    {
+        return new UserResource($user);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(User $user)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateUserRequest $request, User $user)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user)
+    {
+        //
+    }
+}
