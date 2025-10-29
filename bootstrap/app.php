cd79c9928eb419301b6ec117222bb2d652c966e7
Reiduarig <giraudier82@gmail.com>
2025-10-29 18:58:19 +0100

    excepciones globales

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Exceptions\Api\V1\ApiExceptions;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Services\API\V1\ApiResponseService;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Exceptions\ThrottleRequestsException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Validation\ValidationException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,5 +23,37 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+
+        $exceptions->render(function (ThrottleRequestsException $exception)
+        {
+            $retryAfter = data_get($exception->getHeaders(),'Retry-After');
+            $maxAttempts = data_get($exception->getHeaders(),'X-RateLimit-Limit');
+
+            return ApiResponseService::throttled(
+                maxAttempts: $maxAttempts,
+                retryAfter: $retryAfter,
+            );
+        });
+
+        $exceptions->render(function (ModelNotFoundException $exception) {
+            return ApiResponseService::notFound($exception->getMessage());
+        });
+
+        $exceptions->render(function (ValidationException $exception) {
+            return ApiResponseService::validation($exception->getMessage(), $exception->errors());
+        });
+
+        $exceptions->render(function (AuthorizationException $exception) {
+            return ApiResponseService::forbidden($exception->getMessage());
+        });
+
+        $exceptions->render(function (AccessDeniedHttpException $exception) {
+            return ApiResponseService::unauthorized($exception->getMessage());
+        });
+
+        $exceptions->render(function (NotFoundHttpException $exception) {
+            return ApiResponseService::notFound($exception->getMessage());
+        });
+
+        
     })->create();
