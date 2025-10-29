c7b2d5da477affefe2bb27f5a6a6e1f1fd3e6285
Reiduarig <giraudier82@gmail.com>
2025-10-29 18:58:30 +0100

    registrar policies

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,11 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+use App\Models\Ticket;
+use App\Policies\V1\TicketPolicy;
+use App\Models\User;
+use App\Policies\V1\UserPolicy;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::policy(User::class, UserPolicy::class);
+        Gate::policy(Ticket::class, TicketPolicy::class);
     }
 }
